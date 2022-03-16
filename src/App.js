8c42ac3b317eeb624935dd56080d3940430aafc6
mostafa Ahmed Elsayed <83823313+mostafa3537@users.noreply.github.com>
2022-03-17 01:47:25 +0200

    define routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import HomePage from "./pages/homePage/homePage";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return <div>home page</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({title, id, imageUrl, size}) => (
-  <div className={`menu-item ${size} `} >
-    <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}} >
+const MenuItem = ({ title, size, id, imageUrl, linkUrl }) => {
+  const navigate = useNavigate();
 
+  return (
+    <div className={`menu-item ${size} `} onClick={() => navigate(`${linkUrl}`)}>
+    {/* <div className={`menu-item ${size} `} onClick={() => console.log({linkUrl, size, title, imageUrl})}> */}
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
